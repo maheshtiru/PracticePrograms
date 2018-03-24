ed987c9587ae35b3f57aa4236a30c9b3852dcaf1
maheshtiru <tiruveedhula.n@husky.neu.edu>
2018-03-24 17:44:00 -0400

    3 way sort

---
+++ src/com/Sorting/DutchNationalFlag.java
@@ -0,0 +1,61 @@
+package com.Sorting;
+/*
+*****3-WAY partition sorting*****
+* Goal: place same keys together as groups...
+* Quick Sort takes quadratic time for more duplicate keys, coz almost all elements act as pivots
+* Working:
+*   -select any random 'element to compare'(C).
+*   -Three counters-> "lt" which has all elements less than C, to its left..."gt" which has all elements greater than C, to its right...
+*   .."i" which is the traverse counter.
+*   -when arr[i]<C, exchange elements at lt,i and increment both counters
+*   -when arr[i]==C, no exchanges, increment i
+*   -when arr[i]>C, exchange elements at i and gt, decrement gt(do not increment i)
+*   -terminate when "i" meets "gt"
+* Explanation for increments:
+*   At any instance in traversal:
+*       -All elements left to "i" are either equal to (or) less than C. "lt" clearly acts as boundary between two groups. No discrepancies.
+*       so, when i and lt exchange, we can increment i, coz we know that lt is a clear boundary
+*       but, when i and gt exchange, we are not sure whether the new element at i is >C or <C, thus we need to check again. No "i" increment.
+* Recursion:
+*   -C is randomly chosen. So it may be least of the three or greatest of the three. So we need to recurse.
+*   -It is clear that at end of one recursion, elements between lt and gt are grouped together, all equal to C
+*   -So we just need to recurse only on elements before lt and after gt.
+*   $$$--This way of recursion allows grouping even if there are MORE THAN 3 GROUPS---$$$
+*/
+public class DutchNationalFlag<T> {
+
+    public void sort(Comparable<T>[] array){
+        int lo=0,hi=array.length-1;
+        threeWaySort(array,lo,hi);
+    }
+
+    private int compareElements(Comparable<T> x, Comparable<T> y){
+        return x.compareTo((T) y);
+    }
+
+    private void exchange(Comparable<T>[] array, int i, int j){
+        Comparable<T> swap = array[i];
+        array[i]=array[j];
+        array[j]=swap;
+    }
+
+    private void threeWaySort(Comparable<T>[] array,int lo, int hi){
+        if(hi<=lo)return;                         //base case
+
+        int lt=lo, gt=hi, i=lo;
+        Comparable<T> C=array[lo];               //choose element to compare
+
+        while(i<=gt) {
+            if(compareElements(array[i],C)<0)
+                exchange(array,i++,lt++);                   //increment both counters, i and lt
+            else if(compareElements(array[i],C)>0)
+                exchange(array,i,gt--);                     //decrement only the gt counter
+            else
+                i++;
+        }
+
+        //$$$--This way of recursion allows grouping even if there are MORE THAN 3 GROUPS---$$$
+        threeWaySort(array,lo,lt-1);                    //recurse on left side of 'C(element chosen for comparision)' group
+        threeWaySort(array,gt+1,hi);                    //recurse on right side of 'C' group
+    }
+}
--- src/com/Sorting/Main.java
+++ src/com/Sorting/Main.java
@@ -1,7 +1,5 @@
 package com.Sorting;
 
-import java.util.ArrayList;
-
 /*
 Main class for providing inputs to sort algorithms
  */
@@ -13,11 +11,12 @@ public class Main {
         mergeSort();
         quickSort();
         quickSelect();
+        threeWaySort();
     }
 
     //selection sort
     private static void selectionSort(){
-        System.out.println("*****Selection Sort*****");
+        System.out.println("\n"+"\n"+"*****Selection Sort*****");
 
         String[] strArray= {"bbc","zxy","dsd","fef","uyu","ewe","hek","zit","hwk"};
         Integer[] intArray={65,34,98,56,24,96,74,1,51,84,62,75,21,88,56,1,4,315,31,421,512,512,12,57,2,64};
@@ -25,15 +24,19 @@ public class Main {
         SelectionSort<Integer> selectionSortInt = new SelectionSort<>();
         SelectionSort<String> selectionSortString = new SelectionSort<>();
 
+        printDataBeforeOperation(strArray);
         selectionSortString.sort(strArray);
         displayData(strArray);
 
+        System.out.print("\n");
+
+        printDataBeforeOperation(intArray);
         selectionSortInt.sort(intArray);
         displayData(intArray);
     }
     //insertion sort
     private static void insertionSort(){
-        System.out.println("*****Insertion Sort*****");
+        System.out.println("\n"+"\n"+"*****Insertion Sort*****");
 
         String[] strArray= {"bbc","zxy","dsd","fef","uyu","ewe","hek","zit","hwk"};
         Integer[] intArray={65,34,98,56,24,96,74,1,51,84,62,75,21,88,56,1,4,315,31,421,512,512,12,57,2,64};
@@ -41,66 +44,93 @@ public class Main {
         InsertionSort<Integer> insertionSortInt = new InsertionSort<>();
         InsertionSort<String> insertionSortStr = new InsertionSort<>();
 
+        printDataBeforeOperation(strArray);
         insertionSortStr.sort(strArray);
         displayData(strArray);
 
+        System.out.print("\n");
+
+        printDataBeforeOperation(intArray);
         insertionSortInt.sort(intArray);
         displayData(intArray);
     }
     //Shell sort
     private static void shellSort(){
-        System.out.println("*****Shell Sort*****");
+        System.out.println("\n"+"\n"+"*****Shell Sort*****");
 
         //String[] strArray= {"bbc","zxy","dsd","fef","uyu","ewe","hek","zit","hwk"};
         Integer[] intArray={65,34,98,56,24,96,74,1,51,84,62,75,21,88,56,1,4,315,31,421,512,512,12,57,2,64};
+        printDataBeforeOperation(intArray);
 
         ShellSort<Integer> shellSortInt = new ShellSort<>();
-
         shellSortInt.sort(intArray);
         displayData(intArray);
     }
     //merge sort
     private static void mergeSort(){
-        System.out.println("*****Merge Sort*****");
+        System.out.println("\n"+"\n"+"*****Merge Sort*****");
 
         //String[] strArray= {"bbc","zxy","dsd","fef","uyu","ewe","hek","zit","hwk"};
         Integer[] intArray={65,34,98,56,24,96,74,1,51,84,62,75,21,88,56,1,4,315,31,421,512,512,12,57,2,64};
+        printDataBeforeOperation(intArray);
 
         MergeSort<Integer> mergeSortInt = new MergeSort<>();
-
         mergeSortInt.sort(intArray);
         displayData(intArray);
     }
     //quick sort
     private static void quickSort(){
-        System.out.println("*****Quick Sort*****");
+        System.out.println("\n"+"\n"+"*****Quick Sort*****");
 
         String[] strArray= {"bbc","zxy","dsd","fef","uyu","ewe","hek","zit","hwk"};
         Integer[] intArray= {9,10,4,1,3,7,6,8,3};//{65,34,98,56,24,96,74,1,51,84,62,75,21,88,56,1,4,315,31,421,512,512,12,57,2,64};
 
         QuickSort<String> quickSortInt = new QuickSort<>();
 
+        printDataBeforeOperation(strArray);
         quickSortInt.sort(strArray);
         displayData(strArray);
     }
 
     //quick select..select kth smallest element in unordered list
     private static void quickSelect(){
-        System.out.println("*****Quick Select*****");
+        System.out.println("\n"+"\n"+"*****Quick Select*****");
 
         String[] strArray= {"bbc","zxy","dsd","fef","uyu","ewe","hek","zit","hwk"};
         Integer[] intArray= {3,5,1,4,2,8,7,9,6};//all numbers from 1 to 9
+        printDataBeforeOperation(intArray);
 
-        int k=-1;
+        int k=3;
         QuickSelect<Integer> quickSelect = new QuickSelect<>();
 
         System.out.println(k+"th/rd smallest element(starting from 0th): "+quickSelect.select(intArray,k));
         //displayData(strArray);
     }
+
+    //three way sort
+    private static void threeWaySort(){
+        System.out.println("\n"+"*****3-way sort*****");
+
+        //String[] strArray= {"bbc","zxy","dsd","fef","uyu","ewe","hek","zit","hwk"};
+        Integer[] intArray= {9,4,5,6,9,4,4,9,9,4,7,4,6,5,5,5,6,6,7,7};
+        printDataBeforeOperation(intArray);
+
+        DutchNationalFlag<Integer> dutchNationalFlag = new DutchNationalFlag<>();
+        dutchNationalFlag.sort(intArray);
+
+        displayData(intArray);
+    }
+
+    private static void printDataBeforeOperation(Comparable[] arr){
+        System.out.print("before: ");
+        displayData(arr);
+        System.out.print("\n");
+    }
+
     //display method
     private static  void displayData(Comparable[] arr){
-        System.out.println("----display----");
+        //System.out.println("\n");
         for(Comparable c: arr)
-            System.out.println(c);
+            System.out.print(c+" ");
     }
 }
